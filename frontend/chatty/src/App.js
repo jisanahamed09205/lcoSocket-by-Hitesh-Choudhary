a8f53e442e53109b1752ac3d5f247a8c5e19dffd
jisanahamed09205 <jisanahamed09205@gmail.com>
2024-04-14 17:30:01 +0600

    hitesh crash course by socket is over on this project

--- frontend/chatty/src/App.js
+++ frontend/chatty/src/App.js
@@ -7,6 +7,7 @@ import {nanoid} from 'nanoid'
 // const socket = io ('http://localhost:5000')
 //both are same nothing will be much
 const socket = io.connect ('http://localhost:5000')
+const userName = nanoid(4)
 
 function App() {
 
@@ -14,19 +15,24 @@ function App() {
   const [chat, setChat] = useState([])
   const sendChat = (e)=>{
     e.preventDefault()
-    socket.emit("chat",{message})
+    socket.emit("chat",{message, userName})
     setMessage('')
   }
   useEffect(()=>{
     socket.on("chat",(payload)=>{
       setChat([...chat,payload])
     })
-  },[])
+  })
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Chatty</h1>
+        {chat.map((payload,index) => {
+          return(
+            <p key={index}>{payload.message}: <span>id: {payload.userName}</span></p>
+          )
+        })}
         <form onSubmit={sendChat}>
           <input type='text' name='chat' placeholder='send message' value={message} onChange={(e)=>{
             setMessage(e.target.value)
