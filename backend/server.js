677e5e6ad9fbb0560dd65ed3d09baab343eb0a96
jisanahamed09205 <jisanahamed09205@gmail.com>
2024-04-14 12:32:43 +0600

    frontend add and a simple message interface create

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 const app = require ("express") ();
 const server = require ("http").createServer(app);
-const io = require("socket.io")(server)
+const io = require("socket.io")(server,{
+    cors: {
+        origin: "*"
+    }
+})
 io.on("connection",(socket)=>{
     console.log('What is socket: ', socket);
     console.log('Connected');
@@ -9,4 +13,10 @@ io.on("connection",(socket)=>{
         console.log("What is payload: ", payload);
         io.emit("chat",payload)
     })
+})
+
+// app.listen(5000, ()=>console.log("Server is active..."))
+
+server.listen(5000,()=>{
+    console.log("Server is listening at port 5000...");
 })
